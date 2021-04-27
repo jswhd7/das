fe856bd2287bea577128dd5023341fd4f3173e0a
DaHareless1da <jswhd7@gmail.com>
2021-04-27 13:37:03 -0500

    Fresh Nachos

---
+++ src/main/java/com/nachos/das/DasApplication.java
@@ -0,0 +1,13 @@
+package com.nachos.das;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasApplication.class, args);
+	}
+
+}
